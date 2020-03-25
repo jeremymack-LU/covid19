04c25d247e6aec9b15965213046d271c7069a8de
jeremymack-LU <57218089+jeremymack-LU@users.noreply.github.com>
2020-03-25 13:46:08 -0400

    Daily updates

--- covid19_map.R
+++ covid19_map.R
@@ -1,11 +1,21 @@
-url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-19-2020.csv"
+library(dplyr)
+
+url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv"
 
 df <- read.csv(url, sep=",", header=TRUE)
-df.us <- df %>% filter(Country.Region=="US") %>% as.data.frame()
+df.us <- df %>% filter(Country_Region=="US") %>% as.data.frame()
 
-remove <- c("Guam", "Puerto Rico", "Virgin Islands",
-            "United States Virgin Islands", "US", 
-            "Grand Princess", "Diamond Princess")
+remove <- c("Guam", 
+            "Puerto Rico", 
+            "Virgin Islands",
+            "United States Virgin Islands", 
+            "US", 
+            "Grand Princess", 
+            "Diamond Princess",
+            "American Samoa",
+            "Wuhan Evacuee",
+            "Northern Mariana Islands")
 
-df.us2 <- df.us %>% filter(!Province.State %in% remove) %>% as.data.frame()
+df.us2 <- df.us %>% filter(!Province_State %in% remove) %>% group_by(Province_State) %>% summarize(Cases=sum(Confirmed)) %>% as.data.frame()
+
 
