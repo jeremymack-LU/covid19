e247d3f0e5cb5da3dc2b8d789565811e5068af04
jeremymack-LU <57218089+jeremymack-LU@users.noreply.github.com>
2021-05-25 07:23:01 -0400

    updates

--- scripts/covid_updates.R
+++ scripts/covid_updates.R
@@ -1,5 +1,7 @@
 pacman::p_load(tidyverse,cronR)
 
+setwd("/Users/jeremymack/Documents/GitHub/covid19")
+
 # County and State level data from Johns Hopkins CSSE
 ts <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 ts <- read.csv(ts, sep=",", header=TRUE)
